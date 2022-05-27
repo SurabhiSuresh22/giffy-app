724b7f4feab9b2ea1f5a57e421b2b8c6c1653ba9
SurabhiSuresh22 <surahi2207200@gmail.com>
2022-05-27 23:18:03 +0530

    loader added

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,17 @@
 import React,{useEffect, useState} from 'react'
 import './Display.css'
 import axios from 'axios'
+import Loader from './Loader'
 
 const Display = () => {
 
   //store data
 const [data,setData] = useState([])
+const [loading, setLoading] = useState(false)
 
   useEffect(()=>{
     const fetchData = async() =>{
+      setLoading(true)
       const results = await axios('https://api.giphy.com/v1/gifs/trending',{
         params:{
           api_key :'GlVGYHkr3WSBnllca54iNt0yFbjz7L65'
@@ -16,16 +19,21 @@ const [data,setData] = useState([])
       })
       console.log(results);
       setData(results.data.data)
+      setLoading(false)
     }
     fetchData()
   },[])
 
   const renderGif  = () =>{
+
+    if(loading){
+      return <div className="loader"><Loader/></div>
+    }
     return data.map(e =>{
       return (
         <div key={e.id} className="gif">
           Display
-          {/* <img src={e.images.fixed_height.url} alt="" /> */}
+           <img src={e.images.fixed_height.url} alt="" /> 
         </div>
       )
     })
